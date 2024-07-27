0171cb39f32973154ec2e06d13bbe31900cf7d95
JudahDevelops <judahdevelops@gmail.com>
2024-07-27 13:43:07 +0100

    Everything is working properly and the project is officially completed

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-let selected = null;
-
 const calcButton = document.querySelector("#calculate");
 const result = document.querySelector("#results");
 const resetButton = document.querySelector("#reset");
 const results = document.querySelector("#show-results");
-const gender = document.getElementsByName("gender").value;
-    
-   
-
-for (const index in gender) {
-   if (gender[index].checked) {
-            selected = gender[index];
-            break;
-    }
-}
-
 
 function calcBF() {
     const weight = parseInt(document.querySelector("#weight").value);
     const height = parseInt(document.querySelector("#height").value);
     const age = parseInt(document.querySelector("#age").value);
+    const gender = document.getElementsByName("gender");
+    const bmi = weight / height ^ 2;
+    let selected = null; 
+   
+
+    for (const index in gender) {
+    if (gender[index].checked) {
+                selected = gender[index].value;
+                break;
+        }
+    }
     
 
-    const bmi = weight / height ^ 2;
+   
     if (selected === "male") {
-        return (1.20 * bmi) + (0.23 * age);
+        return Math.floor((1.20 * bmi) + (0.23 * age));
     } else if (selected === "female") {
-        return ((1.20 * bmi) + (0.23 * age) - 5.4);
+        return Math.floor((1.20 * bmi) + (0.23 * age) - 5.4);
     } else if (selected === null){
         return "Please select a gender";
     }
